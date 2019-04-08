6af20d817a2f4a7ebb843f712107b03991d56bbd
Raymund Santillan <raymsantillan@gmail.com>
2019-04-09 02:43:37 +0800

    Add docbloc to the base Repository class

--- app/Bio/Repository.php
+++ app/Bio/Repository.php
@@ -2,6 +2,8 @@
 
 namespace Bio;
 
+use Illuminate\Database\Eloquent\Collection;
+
 class Repository {
 
     private $model;
@@ -11,21 +13,44 @@ class Repository {
         $this->model = $model;
     }
 
+    /**
+     * Retrieve all the resource
+     *
+     * @return Collection
+     */
     public function all()
     {
         return $this->model->all();
     }
 
+    /**
+     * Find a specified resource
+     *
+     * @param int $id
+     * @return Collection
+     */
     public function find($id)
     {
         return $this->model->findOrFail($id);
     }
 
+    /**
+     * Get a resource by enumerating attributes
+     *
+     * @param array $query
+     * @return Collection
+     */
     public function enumerate($query = [])
     {
         return $this->model->where($query)->first();
     }
 
+    /**
+     * Create a certain resource
+     *
+     * @param array $resource
+     * @return Collection
+     */
     public function create(array $resource)
     {
         return $this->model->create($resource);
